3783b43cdf2c17680792bafd22c9c1f3fe0bdede
Emily Salinas <emilyrh1058@gmail.com>
2021-07-06 21:47:53 -0500

    updated open link to resume and list of learn tech

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,8 +6,19 @@ function Resume() {
       <div className='container d-flex flex-column text-center'>
         <div className='p-2 text-center'>
           <h6 id='resume'><a href='https://docs.google.com/document/d/1AXSaOtvUjvCeRH2habJSuHwvBLbSFfuRK17R3Z7l9KA/edit?usp=sharing' target='blank' download>Download PDF</a></h6>
-          
         </div>
+        <br></br>
+        <ul>Technologies learned:
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>Node</li>
+          <li>NPM</li>
+          <li>SQL</li>
+          <li>React</li>
+          <li>MERN</li>
+          <li>To be continued...</li>
+        </ul>
       </div>
       <div className='p-2'>
         
